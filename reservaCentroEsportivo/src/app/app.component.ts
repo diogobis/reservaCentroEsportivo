5cfddbf97143132cc8acb8aa1425b6e81db4089f
Diogo Alves <diogobis23@gmail.com>
2024-05-11 11:49:46 -0300

    Frontend - Remove Angular template and add routes

--- reservaCentroEsportivo/src/app/app.component.ts
+++ reservaCentroEsportivo/src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { UsuarioService } from './services/usuario/usuario.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './assets/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    NavbarComponent,
+  ],
+  providers: [UsuarioService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'reservaCentroEsportivo';
+
+  constructor(private router: Router) {
+    if (!localStorage.getItem('user')) router.navigate(['login']);
+  }
 }
