f0312dbfd1c2ab7575b5333fbc4aa420cb56b579
Diogo Alves <diogobis23@gmail.com>
2024-05-27 16:51:51 -0300

    Frontend: Configure MSAL login

--- reservaCentroEsportivo/src/app/app.component.ts
+++ reservaCentroEsportivo/src/app/app.component.ts
@@ -3,8 +3,71 @@ import { Router, RouterOutlet } from '@angular/router';
 import { UsuarioService } from './services/usuario/usuario.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './assets/navbar/navbar.component';
+import {
+  MSAL_GUARD_CONFIG,
+  MSAL_INSTANCE,
+  MSAL_INTERCEPTOR_CONFIG,
+  MsalGuardConfiguration,
+  MsalInterceptor,
+  MsalInterceptorConfiguration,
+} from '@azure/msal-angular';
+import {
+  BrowserCacheLocation,
+  IPublicClientApplication,
+  InteractionType,
+  LogLevel,
+  PublicClientApplication,
+} from '@azure/msal-browser';
+
+export function loggerCallback(logLevel: LogLevel, message: string) {
+  console.log(message);
+}
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: '9d300292-b730-435d-849f-51e19713b7cf',
+      authority: 'https://login.microsoftonline.com/common/',
+      redirectUri: '/',
+      postLogoutRedirectUri: '/',
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+    },
+    system: {
+      allowNativeBroker: false, // Disables WAM Broker
+      loggerOptions: {
+        loggerCallback,
+        logLevel: LogLevel.Info,
+        piiLoggingEnabled: false,
+      },
+    },
+  });
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', [
+    'user.read',
+  ]);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap,
+  };
+}
+
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return {
+    interactionType: InteractionType.Redirect,
+    authRequest: {
+      scopes: ['user.read'],
+    },
+    loginFailedRoute: '/login-failed',
+  };
+}
 
 @Component({
   selector: 'app-root',
@@ -16,7 +79,26 @@ import { NavbarComponent } from './assets/navbar/navbar.component';
     HttpClientModule,
     NavbarComponent,
   ],
-  providers: [UsuarioService],
+  providers: [
+    UsuarioService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory,
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory,
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory,
+    },
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- reservaCentroEsportivo/src/app/pages/login/login.component.ts
+++ reservaCentroEsportivo/src/app/pages/login/login.component.ts
@@ -1,23 +1,62 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UsuarioService } from '../../services/usuario/usuario.service';
 import { Router } from '@angular/router';
+import { MsalModule, MsalService } from '@azure/msal-angular';
+import { Configuration, PublicClientApplication } from '@azure/msal-browser';
+import { lastValueFrom } from 'rxjs';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMicrosoft } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, MsalModule, FontAwesomeModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  providers: [MsalService],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public faMicrosoft = faMicrosoft;
+
   public login = {
     RA: '',
     senha: '',
   };
 
-  constructor(private router: Router, private usuarioService: UsuarioService) {}
+  constructor(
+    private router: Router,
+    private usuarioService: UsuarioService,
+    private msalService: MsalService
+  ) {
+  }
+
+  async ngOnInit() {
+    await lastValueFrom(this.msalService.initialize());
+  }
+
+  public loginMicrosoft() {
+    try {
+      this.msalService.loginPopup().subscribe((result) => {
+        let user = result.account;
+        console.log(user);
+        this.usuarioService
+          .get({ email: user.username })
+          .then((result: any) => {
+            localStorage.setItem('user', JSON.stringify(result[0]));
+            this.router.navigate(['home']);
+          })
+          .catch((error) => {
+            console.log(error);
+            alert('Login falhou');
+          });
+      });
+    } catch (error) {
+      console.log(error);
+      alert('Login falhou');
+    }
+  }
 
   public doLogin() {
     this.usuarioService.get(this.login).then((result: any) => {
