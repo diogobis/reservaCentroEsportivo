7f1bd63b2407223455ca0bcc05e42e1881ffa964
Diogo Alves <diogobis23@gmail.com>
2024-06-07 12:36:10 -0300

    Backend: add post, put and delete to BaseApplication

--- backend/src/applications/BaseApplication/BaseApplication.js
+++ backend/src/applications/BaseApplication/BaseApplication.js
@@ -22,8 +22,46 @@ class BaseApplication {
     });
 
     app.post(`/${table}`, (req, res) => {
-      console.log(req,res);
-    })
+      let body = req.body;
+
+      let sql = `INSERT INTO ${table}(${Object.keys(body).join(
+        ","
+      )}) VALUES (${Object.keys(body)
+        .map((key) => {
+          return `'${body[key]}'`;
+        })
+        .join(",")})`;
+
+      db.query(sql, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+      });
+    });
+
+    app.put(`/${table}/:KEY`, (req, res) => {
+      let KEY = req.params.KEY;
+
+      let sql = `UPDATE ${table} SET ${Object.keys(reserva)
+        .map((key) => {
+          return `${key} = '${reserva[key]}'`;
+        })
+        .join(",")} WHERE ID = '${KEY}'`;
+
+      db.query(sql, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+      });
+    });
+
+    app.delete(`/${table}/:KEY`, (req, res) => {
+      let KEY = req.params.KEY;
+
+      let sql = `DELETE FROM ${table} WHERE ID = '${KEY}'`;
+      db.query(sql, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+      });
+    });
   }
 }
 
