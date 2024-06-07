4cfd783d1a6513f3e331249aaef94b7e0c53f901
Diogo Alves <diogobis23@gmail.com>
2024-06-07 14:22:44 -0300

    Frontend: Login validation

--- reservaCentroEsportivo/src/app/pages/login/login.component.ts
+++ reservaCentroEsportivo/src/app/pages/login/login.component.ts
@@ -1,23 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UsuarioService } from '../../services/usuario/usuario.service';
 import { Router } from '@angular/router';
 import { MsalModule, MsalService } from '@azure/msal-angular';
-import { Configuration, PublicClientApplication } from '@azure/msal-browser';
-import { lastValueFrom } from 'rxjs';
+import { Subject, lastValueFrom } from 'rxjs';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMicrosoft } from '@fortawesome/free-brands-svg-icons';
+
+import { NgbAlert, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { debounceTime, tap } from 'rxjs/operators';
+
+// @ts-ignore
+const $: any = window['$'];
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule, MsalModule, FontAwesomeModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    MsalModule,
+    FontAwesomeModule,
+    NgbAlertModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   providers: [MsalService],
 })
 export class LoginComponent implements OnInit {
+  private _message$ = new Subject<string>();
+  successMessage = '';
+
+  @ViewChild('selfClosingAlert', { static: false })
+  selfClosingAlert: NgbAlert = new NgbAlert();
+
   public faMicrosoft = faMicrosoft;
 
   public login = {
@@ -29,7 +48,15 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private usuarioService: UsuarioService,
     private msalService: MsalService
-  ) {}
+  ) {
+    this._message$
+      .pipe(
+        takeUntilDestroyed(),
+        tap((message) => (this.successMessage = message)),
+        debounceTime(5000)
+      )
+      .subscribe(() => this.selfClosingAlert?.close());
+  }
 
   async ngOnInit() {
     await lastValueFrom(this.msalService.initialize());
@@ -42,8 +69,12 @@ export class LoginComponent implements OnInit {
         this.usuarioService
           .get({ email: user.username })
           .then((result: any) => {
-            localStorage.setItem('user', JSON.stringify(result[0]));
-            this.router.navigate(['home']);
+            if (result[0]) {
+              localStorage.setItem('user', JSON.stringify(result[0]));
+              this.router.navigate(['home']);
+            } else {
+              this._message$.next(`Login falhou. Tente novamente.`);
+            }
           })
           .catch((error) => {
             console.error(error);
