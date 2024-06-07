d7da6e4b151e94b44f7c63fb0bb15dc8a0e50d74
Diogo Alves <diogobis23@gmail.com>
2024-06-07 20:07:49 -0300

    Frontend: update routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { NovaReservaComponent } from './pages/nova-reserva/nova-reserva.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'nova-reserva', component: NovaReservaComponent}
